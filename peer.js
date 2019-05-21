fb552a63ff3e94eeec42110985c4b560c181e8a9
skurtjean <48225257+skurtjean@users.noreply.github.com>
2019-05-20 21:15:53 -0300

    First

---
+++ peer.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').Server(app);
+const ExpressPeerServer = require('peer').ExpressPeerServer;
+const MongoClient = require('mongodb').MongoClient;
+const io = require('socket.io')(http);
+const crypto = require('crypto');
+const uri = "mongodb+srv://Voicejj:Voicejj2019@voicejj-dszcc.mongodb.net/test?retryWrites=true"
+var users = [];
+
+app.set('view engine', 'ejs');
+/*
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://vps13171.publiccloud.com.br");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});*/
+app.use(session({secret: 'Voicejj',saveUninitialized: true,resave: true}))
+app.use(express.static(__dirname + '/assets'));
+app.use(bodyParser.urlencoded({extended:true}));
+var userslog = [];
+
+var server = require('http').createServer(app);
+var peerserver = ExpressPeerServer(server, { debug: true });
+app.use('/peerjs', peerserver);
+
+io.on('connection', function(socket){
+    console.log('O maot é um vagabundo');
+    /*socket.on('join', function(data){
+        users[data.from] = socket;
+    });
+    socket.on('disconnect', function(){
+        console.log('O maot é um otário');
+    });*/
+    socket.on('sendMessage', function(data){
+        console.log("O Nicholas é um merda");
+        console.log(data)
+        io.emit("receiveMessage", data.message);
+    });/*
+    socket.on('typing', function(data){
+        console.log("O Jean é um inutil");
+        if (data.typing) {
+            users[data.to].emit("istyping");
+        } else {
+            users[data.to].emit("notyping");
+        }
+    });*/
+});
+
+
+
+//funções
+function autentica(data){
+    if(!(data in userslog)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+
+
+//urls
+app.get('/', function(req, res) { 
+    res.render('Home.ejs'); 
+});
+app.get('/login', function(req, res) { 
+    res.render('Login.ejs');
+});
+app.post('/logar', function(req, res) { 
+    var username = req.body.user;
+    var password = crypto.createHash('md5').update(req.body.pass).digest('hex');
+    let cursor = db.collection('users').find({"username": username, "password": password}).toArray((err, results) => {
+        if (err) return console.log(err);
+        userslog[results[0]._id] = req.session;
+        userslog[results[0]._id]._id = results[0]._id;
+        userslog[results[0]._id].email = results[0].email;
+        userslog[results[0]._id].user = results[0].username;
+        res.redirect('/lista');
+    });
+});
+app.get('/cadastro', function(req, res) { 
+    res.render('Cadastro.ejs');
+});
+app.post('/cadastrar', function(req, res) {
+    var email = req.body.email;
+    var user = req.body.user;
+    var password = crypto.createHash('md5').update(req.body.pass).digest('hex');
+    var users = new Object();
+    users.email = email;
+    users.username = user;
+    users.password = password;
+    db.collection('users').insertOne(users, (err, result) => {
+        if (err) return console.log(err);
+        userslog[result.ops[0]._id] = req.session;
+        userslog[result.ops[0]._id]._id = result.ops[0]._id;
+        userslog[result.ops[0]._id].email = result.ops[0].email;
+        userslog[result.ops[0]._id].user = result.ops[0].username;
+        res.redirect('/lista');
+    });
+});
+app.get('/lista', function(req, res){
+    console.log(userslog);
+    if(autentica(req.session._id)){
+        res.render('Lista.ejs', {users: userslog});
+    }
+    else{
+        res.redirect('/login');
+    }
+});
+
+
+
+
+
+//Portas de cada um
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.log(err);
+    db = client.db('Voicejj');    
+    app.listen(80, function(){
+        console.log('Express');
+    });
+});
+
+server.listen(9000, function(){
+    console.log('Peer');
+});
+
+http.listen(9001, function(){
+    console.log('Socket');
+});
---
+++ view/js/components/ComponentTexto.vue
@@ -0,0 +1,116 @@
+<template>
+    <div id="chat-frame-box">
+        <div class="status status-done">
+             <div v-if="finished" class="mb-4">
+                <script>
+                    alert('Chat finalizado.')
+                </script>
+            </div>
+        </div>
+
+        <div class="talking-area">
+            <div v-for="(item, index) in messages" :key="index" :class="item.fromUserId == sender.id ? 'msg agent-me' : 'msg agent-notme'">
+                <div class="text">
+                    <span class="name"> {{item.fromUserId == to ? to : from }} </span>
+                    {{ item.message }}
+                </div>
+            </div>
+        </div>
+        <div class="panel-text">
+            <p class="typing" v-if="typing">{{ receiver.name }} está digitando...</p>
+            <textarea v-model="message" @keyup.enter="sendMessage" @keypress="onTyping" @keyup.delete="stopTyping" id="message" placeholder="Enviar mensagem..." ></textarea>
+            <button @click="sendMessage">Enviar Mensagem</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id', 'to'],
+    data () {
+        return {
+            message: '',
+            messages: [],
+            typing: false,
+            finished: false,
+            socket: io('http://vps13171.publiccloud.com.br:9001')
+        }
+    },
+    created() {
+        this.socket.emit("join", {
+            from: this.from
+        });
+    },
+    mounted() {
+        //Listen for messages
+        this.socket.on('receiveMessage', this.receiveMessage);
+        this.socket.on('istyping', this.someoneIsTyping);
+        this.socket.on('notyping', this.finishIsTyping);
+        this.socket.on('finishing', this.finishingChat);
+    },
+    destroyed() {
+    },
+    methods: {
+        sendMessage() {
+            if (this.message.trim().length > 0) {
+                let messagePackage = this.createMsgObj(this.message);
+                this.socket.emit('sendMessage', messagePackage);
+                this.socket.emit("typing", {to: this.to, name: this.from, typing:false });
+                this.messages.push(messagePackage);
+                this.storeMessage();
+                this.message = "";
+                this.scrollToBottom();
+            }else{
+                alert("Digite algo antes de enviar :)");
+            }
+        },
+        receiveMessage(msg) {
+            this.messages.push(msg);
+            this.scrollToBottom();
+        },
+        onTyping() {
+            if(this.message.length == 1 || (this.message.length%100 == 0 && this.message.length > 0))
+            {
+                this.socket.emit("typing", {toUserId: this.to, name: this.from, typing: true });
+            }
+        },
+        stopTyping() {
+            if(this.message.length == 0)
+            {
+                this.socket.emit("typing", {toUserId: this.to, name: this.from, typing:false });
+            }
+        },
+        someoneIsTyping(data) {
+            this.typing = true;
+        },
+        finishIsTyping(data) {
+            this.typing = false;
+        },
+        createMsgObj() {
+            return {
+                fromUserId: this.from,
+                toUserId: this.to,
+                message: this.message
+            }
+        },
+        scrollToBottom() {
+            setTimeout(function() {
+                document.querySelector('.talking-area').scrollTop = document.querySelector('.talking-area').scrollHeight
+            },300)
+        },
+        finishingChat(){
+            this.finished = true;
+            console.log("chamada finalizada");
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .typing {
+        font-family: 'Open Sans', sans-serif;
+        font-size: 14px;
+        color: #c7bdbd;
+        margin-bottom: 10px;
+    }
+</style>
