1fb611dd3d0fbf5cf964300f890e8b3fc9db1d15
Jean Skurtinski <jeanskurtinski@gmail.com>
2019-06-05 23:17:08 -0300

    Chat em grupo funcionando (e só ele mesmo)

--- assets/vuejs/main.js
+++ assets/vuejs/main.js
@@ -1,8 +1,72 @@
-import Vue from 'vue/dist/vue';
-
 window.Vue = Vue;
-Vue.component('component-chat-texto', require('./components/ComponentTexto.vue'));
+
+Vue.component('grupo',{
+    template: `
+        <div class="chat" id="container-chat">
+            <ul class="topbar" id="topbar">
+                <li class="topbar-item-li"> <a class="topbar-item" id="video-call">📹 </a></li>
+                <li class="topbar-item-li"> <a class="topbar-item" id="audio-call">📞</a></li>
+                <p> Conversando no grupo {{ to }} </p>
+            </ul>
+            <div id="messages">
+                <div v-for="(item, index) in messages" :key="index" class="message">
+                    <div class="autor">{{ item.fromUsername }}</div>
+                    <div class="messagebody">{{ item.message }}</div>
+                    <hr class="sepadadormensagem">
+                </div>
+            </div>
+            <p class="digitando"> O corno está digitando </p>
+            <div class="container">
+                <textarea v-model="message" @keydown.enter="sendMessage" class="textochat" id="textbox"></textarea>
+                <button @click="sendMessage" class="enviarmensagem" id="send">↩</button>
+            </div>
+        </div>`,
+    props: {'from': String, 'to': String},
+    data: function(){
+        return {
+            message: '',
+            messages: [],
+            socket: io('localhost:9001'),
+        }
+    },
+    created(){
+        this.socket.emit('join', {_id : this.from});
+    },
+    mounted(){
+        this.socket.on('receiveMessage', this.receiveMessage);
+    },
+    destroyed(){
+        this.socket.emit('disconnect', this.from);
+    },
+    methods: {
+        sendMessage() {
+            if (this.message.trim().length > 0) {
+                let messagePackage = this.createMsgObj(this.message);
+                this.socket.emit('sendMessage', messagePackage);
+                this.message = "";
+            }else{
+                alert("Digite algo antes de enviar :)");
+            }
+        },
+        receiveMessage(msg) {
+            this.messages.push(msg);
+            this.scrollToBottom();
+        },
+        createMsgObj() {
+            return {
+                fromUserId: this.from,
+                to: this.to,
+                message: this.message
+            }
+        },
+        scrollToBottom() {
+            setTimeout(function() {
+                document.querySelector('#messages').scrollTop = document.querySelector('#messages').scrollHeight
+            },300)
+        }
+    }
+});
 
 const app = new Vue({
-    el: '#vue-app'
+    el: '#app'
 });
